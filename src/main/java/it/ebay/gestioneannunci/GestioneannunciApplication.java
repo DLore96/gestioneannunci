35146cde59b55facfc4c0956038f2f5ffbfc709d
Andrea Vecchiato <vecchiatoandry@gmail.com>
2021-05-13 17:08:39 +0200

    firs commit

---
+++ src/main/java/it/ebay/gestioneannunci/GestioneannunciApplication.java
@@ -0,0 +1,13 @@
+package it.ebay.gestioneannunci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneannunciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneannunciApplication.class, args);
+	}
+
+}
---
+++ src/main/java/it/ebay/gestioneannunci/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.ebay.gestioneannunci;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GestioneannunciApplication.class);
+	}
+
+}
